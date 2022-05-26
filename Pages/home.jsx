973cf06bd80fb5fc93fe7ccf21e0dce1396a0430
billracks <50613921+billracks@users.noreply.github.com>
2022-05-26 18:03:05 +0200

    Add files via upload

---
+++ Pages/home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./home.scss"
+
+export const home = () => {
+  return (
+    <div className='home'>
+        <h2>Metro Eye</h2>
+    </div>
+  )
+}
+export default home 
